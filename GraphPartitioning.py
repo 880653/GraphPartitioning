291302f8592db598647e89676359fa59fd7e1eb6
Irati <irapuyjau@gmail.com>
2021-11-22 10:49:19 +0100

    Txukunketaks

--- GraphPartitioning.py
+++ GraphPartitioning.py
@@ -2,7 +2,21 @@ import numpy as np
 import random
 
 
-def ObjectiveFunctionValue(n,m,v):
+def chargeMatrix():
+    m = np.loadtxt('Examples/Adibide1.txt', skiprows=1)
+    print("Having the neighbour matrix:\n",m)
+    return m
+
+def randoms(size):
+    # create a vector of 1s, and generate random positions where to put 0s
+    v = np.ones(size, dtype=int)
+    positions = random.sample(range(0, size), int(size/2))
+    v[positions] = 0
+    
+    print("and the random initial solution:\n", v)
+    return v
+
+def ObjectiveFunctionValue(n, v, m):
     sum = 0
     for i in range (n):
         for j in range (i, n):
@@ -10,27 +24,13 @@ def ObjectiveFunctionValue(n,m,v):
     print("We get the initial sum:", sum, "\n")
     return sum
 
-def randoms():
-    # read the matrix provided and its size
-    size = int(np.loadtxt('Examples/Adibide1.txt', max_rows = 1))
-    m = np.loadtxt('Examples/Adibide1.txt', skiprows=1)
-    
-    # create a vector of 1s, and generate random positions where to put 0s
-    v = np.ones(size, dtype=int)
-    positions = random.sample(range(0, size), int(size/2))
-    v[positions] = 0
-    
-    print("Having the neighbour matrix:\n",m)
-    print("and the random solution:\n", v)
-    return size,m,v
-
 def swap(actual, i, j):
     aux = actual.copy()
     aux[i] = actual[j]
     aux[j] = actual[i]
     return aux
 
-def ObjectiveFunctionValueWithActual(i, j, actual, new, actualValue):
+def ObjectiveFunctionValueWithActual(i, j, new, actualValue, m, n):
     value = actualValue.copy()
 
     for index in range(n):
@@ -48,43 +48,26 @@ def ObjectiveFunctionValueWithActual(i, j, actual, new, actualValue):
     return value
 
 
-# def NewFunctionValue(actualSolution, newSolution, actualValue):
-#     newValue = actualValue.copy()
-#     changes = (actualSolution - newSolution).copy()
-#     print(changes, "changes")
-    
-#     for i in changes:
-#         if ((changes[i]!=1) & (changes[i]!=-1)):
-#             if(actualSolution[i] == 1):
-#                 pass
-#             elif(actualSolution[i] == -1):
-#                 pass
-#             else:
-#                 pass
-#     return newValue
-            
-    
-    
 
-def totalNeighbourhood(n, actualSolution, actualValue):
+def GraphPartitioning():
+    m = chargeMatrix()
+    n = len(m)
+    actualSolution = randoms(n)
+    actualValue = ObjectiveFunctionValue(n, actualSolution, m)
     newSolution=[]
     for i in range(0, n):
         for j in range(i+1, n):
             if (actualSolution[i] != actualSolution[j]):
                 newSolution = swap(actualSolution, i, j)
-                newValue = ObjectiveFunctionValueWithActual(i, j, actualSolution, newSolution, actualValue)
+                newValue = ObjectiveFunctionValueWithActual(i, j, newSolution, actualValue, m, n)
                 if(newValue < actualValue):
                     actualSolution = newSolution
                     actualValue = newValue
     return actualSolution, actualValue
 
-###############################################################################
+########################          EXECUTION          ########################
 
-n,m,v = randoms()
-value = ObjectiveFunctionValue(n,m,v)
-
-
-solution, sum2 = totalNeighbourhood(n, v, value)
+solution, value = GraphPartitioning()
 print("But if we look for local optimum we get the solution \n", solution)
-print("and the sum:", sum2)
+print("and the sum:", value)
 

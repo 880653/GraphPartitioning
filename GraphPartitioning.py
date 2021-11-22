1389710a3911fe48d2bcae6712425ae2110c085c
Eneko <eneko.atxa@opendeusto.es>
2021-11-22 16:52:03 +0100

    merge

--- GraphPartitioning.py
+++ GraphPartitioning.py
@@ -7,15 +7,15 @@ def ObjectiveFunctionValue(n,m,v):
     for i in range (n):
         for j in range (i, n):
             sum += (v[i]*(1-v[j]) + v[j]*(1-v[i]))*m[i][j]
-    # print("We get the sum:", sum)
+    print("We get the sum:", sum)
     return sum
 
 def randoms():
-    # read the matrix provided and its size
+    #read the matrix provided and its size
     size = int(np.loadtxt('Examples/Adibide1.txt', max_rows = 1))
     m = np.loadtxt('Examples/Adibide1.txt', skiprows=1)
     
-    # create a vector of 1s, and generate random positions where to put 0s
+    #create a vector of 1s, and generate random positions where to put 0s
     v = np.ones(size, dtype=int)
     positions = random.sample(range(0, size), int(size/2))
     v[positions] = 0
@@ -30,41 +30,31 @@ def swap(actual, i, j):
     aux[j] = actual[i]
     return aux
 
-def ObjectiveFunctionValueWithActual(i, j, actual, new, actualValue):
+
+def ObjectiveFunctionValueWithActual(actual, new, actualValue):
     value = actualValue.copy()
-
-    for index in range(np.size(actual)):
-        if(actual[index] == actual[i] & i != index):
-            value -= m[actual[index], i]
-        else:
-            value += m[actual[index], i]
-    
-    valuetemp = ObjectiveFunctionValue(n, m, new)
-    return valuetemp
-
-# def ObjectiveFunctionValueWithActuala(actual, new, actualValue):
-#     value = actualValue.copy()
-#     #if we have a 1 or a -1, we have changed that node
-#     changes=actual-new
-#     for permutation in changes:
-#         if(permutation==1 or permutation==-1):
-#             for x in actual:
-#                 if(x==actual[np.where(permutation)[0][0]]):
-#                     print(actual[np.where(permutation)[0][0]], "x")
-
-#                     value-=m[np.where(x)[0][0], np.where(permutation)[0][0]]
-#                 else:
-#                     value+=m[np.where(x)[0][0], np.where(permutation)[0][0]]
-#     return value
+    #if we have a 1 or a -1, we have changed that node
+    changes=actual-new
+    for permutation in changes:
+        if(permutation==1 or permutation==-1):
+            for x in actual:
+                
+                if(x==actual[np.where(permutation)[0][0]]):
+                    value-=m[x, np.where(permutation)[0][0]]
+                else:
+                    value+=m[x, np.where(permutation)[0][0]]
+    print(value, "txarra")
+    print(ObjectiveFunctionValue(n, m, new), "ona")
+    return value
 
 
-def NewFunctionValue(actualSolution, newSolution, actualValue):
-    newValue = actualValue.copy()
-    changes = (actualSolution - newSolution).copy()
-    print(changes, "patata")
-    
-    for i in actualSolution:
-        if (changes[i]==1 )
+#def NewFunctionValue(actualSolution, newSolution, actualValue):
+#    newValue = actualValue.copy()
+#    changes = (actualSolution - newSolution).copy()
+#    print(changes, "patata")
+#    
+#    for i in actualSolution:
+#        if (changes[i]==1 )
     
     
 
@@ -74,13 +64,13 @@ def totalNeighbourhood(n, actualSolution, actualValue):
         for j in range(i+1, n):
             if (actualSolution[i] != actualSolution[j]):
                 newSolution = swap(actualSolution, i, j)
-                newValue = NewFunctionValue(actualSolution, newSolution, actualValue)
+                newValue = ObjectiveFunctionValueWithActual(actualSolution, newSolution, actualValue)
                 if(newValue < actualValue):
                     actualSolution = newSolution
                     actualValue = newValue
     return actualSolution, actualValue
 
-###############################################################################
+
 
 n,m,v = randoms()
 value = ObjectiveFunctionValue(n,m,v)
